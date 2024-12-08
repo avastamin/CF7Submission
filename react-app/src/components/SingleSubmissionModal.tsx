f86c3c8f26ed3b9981114ce590ab9fc178418474
Ruhul Amin <r_amin105@yahoo.com>
2024-12-08 21:21:19 +0600

    render on modal

--- react-app/src/components/SingleSubmissionModal.tsx
+++ react-app/src/components/SingleSubmissionModal.tsx
@@ -1,12 +1,6 @@
 "use client";
 
-import {
-  Dialog,
-  DialogBackdrop,
-  DialogPanel,
-  DialogTitle,
-} from "@headlessui/react";
-import { CheckIcon } from "@heroicons/react/24/outline";
+import { Dialog, DialogBackdrop, DialogPanel } from "@headlessui/react";
 import { Submission } from "../types";
 
 export default function SingleSubmissionModal({
@@ -32,24 +26,30 @@ export default function SingleSubmissionModal({
             className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-sm sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
           >
             <div>
-              <div className="mx-auto flex size-12 items-center justify-center rounded-full bg-green-100">
-                <CheckIcon
-                  aria-hidden="true"
-                  className="size-6 text-green-600"
-                />
+              <div className="px-4 sm:px-0">
+                <h3 className="text-base/7 font-semibold text-gray-900">
+                  Applicant Information
+                </h3>
+                <p className="mt-1 max-w-2xl text-sm/6 text-gray-500">
+                  Personal details and application.
+                </p>
               </div>
-              <div className="mt-3 text-center sm:mt-5">
-                <DialogTitle
-                  as="h3"
-                  className="text-base font-semibold text-gray-900"
-                >
-                  Submission Details
-                </DialogTitle>
-                <div className="mt-2">
-                  <p className="text-sm text-gray-500">
-                    {JSON.stringify(selectedSubmission?.submission_data)}
-                  </p>
-                </div>
+              <div className="mt-6 border-t border-gray-100">
+                <dl className="divide-y divide-gray-100">
+                  {selectedSubmission?.submission_data &&
+                    Object.entries(selectedSubmission.submission_data).map(
+                      ([key, value]) => (
+                        <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+                          <dt className="text-sm/6 font-medium text-gray-900">
+                            {key}
+                          </dt>
+                          <dd className="mt-1 text-sm/6 text-gray-700 sm:col-span-2 sm:mt-0">
+                            {Array.isArray(value) ? value.join(", ") : value}
+                          </dd>
+                        </div>
+                      )
+                    )}
+                </dl>
               </div>
             </div>
             <div className="mt-5 sm:mt-6">
